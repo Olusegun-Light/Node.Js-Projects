d6829a1e595f4b290a319ea6d277e353f7b91418
Light <lightolusegun1@gmail.com>
2023-07-02 16:27:57 +0100

    data validation

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+
 // Describing a schema
 const tourSchema = new mongoose.Schema(
   {
@@ -9,6 +10,14 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a name"],
       unique: true,
       trim: true,
+      maxlength: [
+        40,
+        "A tour name must have less or equal than 40 characters ",
+      ],
+      minlength: [
+        10,
+        "A tour name must have more or equal than 10 characters ",
+      ],
     },
     slug: String,
     duration: {
@@ -22,10 +31,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either easy medium or difficult",
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
     },
     ratingsQuantity: {
       type: Number,
@@ -56,7 +71,7 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
-    secreatTour: {
+    secretTour: {
       type: Boolean,
       default: false,
     },
@@ -100,7 +115,14 @@ tourSchema.pre(/^find/, function (next) {
 // Post middleware for find
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now - this.start}  milliseconds!`);
-  console.log(docs);
+  // console.log(docs);
+  next();
+});
+
+// Aggregation Middleware
+tourSchema.pre("aggregate", function (next) {
+  this.pipeline().unshift({ $match: { secreteTour: { $ne: true } } });
+  console.log(this.pipeline());
   next();
 });
 
